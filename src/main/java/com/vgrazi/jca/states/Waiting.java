85d0ecf790640445dad3bc6ded5de06d41dd3d31
Victor Grazi <vgrazi@gmail.com>
2019-09-24 20:03:47 -0400

    waiting state now centers thread in mutex

--- src/main/java/com/vgrazi/jca/states/Waiting.java
+++ src/main/java/com/vgrazi/jca/states/Waiting.java
@@ -3,12 +3,21 @@ package com.vgrazi.jca.states;
 import com.vgrazi.jca.context.RelativePosition;
 import com.vgrazi.jca.context.ThreadContext;
 import com.vgrazi.jca.context.ThreadSprite;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
-public class Waiting extends State {
+public class Waiting extends State implements InitializingBean {
     @Autowired
     ThreadContext threadContext;
+    @Value("${monolith-left-border}")
+    private int monolithLeftBorder;
+    @Value("${monolith-right-border}")
+    private int monolithRightBorder;
+    @Value("${arrow-length}")
+    private int arrowLength;
 
+    private int xPos;
     @Override
     public void advancePosition(ThreadSprite sprite) {
         RelativePosition relativePosition = sprite.getRelativePosition();
@@ -18,9 +27,16 @@ public class Waiting extends State {
                 break;
             case At:
             case In:
+                sprite.setXPosition(xPos);
+                break;
             case After:
                 // nothing to do, this thread is waiting
                 break;
         }
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        xPos = (monolithLeftBorder + monolithRightBorder + arrowLength)/2;
     }
 }
