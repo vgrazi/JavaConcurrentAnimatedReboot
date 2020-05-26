ad115ac2fa72ecd9eaa6ca4d3cb283eed4c2c50a
Victor Grazi <vgrazi@gmail.com>
2020-05-25 22:28:26 -0400

    Added all threads to BasicSlide, added SpritesList.jpg showing all Sprite types

--- readme.md
+++ readme.md
@@ -3,3 +3,6 @@ This is a revised and much simplified version of Java Concurrent Animated
 In the original version too much control was taken by the canvas, and all of the interactions were dependent on the slide program.
 
 In this reboot version, there is a single runnable per slide that carries all of the threads through the desired states. The renderer iterates through all of the threads, gets their state, current position, and shape, and renders them.
+
+Sprites List:
+![image](src/main/resources/images/SpritesList.jpg)
--- src/main/java/com/vgrazi/jca/slides/BasicSlide.java
+++ src/main/java/com/vgrazi/jca/slides/BasicSlide.java
@@ -1,6 +1,6 @@
 package com.vgrazi.jca.slides;
 
-import com.vgrazi.jca.sprites.ThreadSprite;
+import com.vgrazi.jca.sprites.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
@@ -27,20 +27,64 @@ public class BasicSlide extends Slide {
         reset();
         threadContext.addButton("Add running thread", () -> {
             // create a new sprite
-            ThreadSprite sprite = (ThreadSprite) applicationContext.getBean("runnerThreadSprite");
+            ObjectSprite objectSprite = (ObjectSprite) applicationContext.getBean("objectSprite");
+            attachAndStartRunnable(objectSprite);
+            objectSprite.setMessage("ObjectSprite");
+            threadContext.addSprite(objectSprite);
+            threadContext.addYPixels(10);
+            ThreadSprite sprite = (ThreadSprite) applicationContext.getBean("threadSprite");
+            attachAndStartRunnable(sprite);
+            sprite.setMessage("ThreadSprite");
+            threadContext.addSprite(sprite);
+            threadContext.addYPixels(10);
+            RunnerThreadSprite runnerThreadSprite = (RunnerThreadSprite) applicationContext.getBean("runnerThreadSprite");
+            attachAndStartRunnable(runnerThreadSprite);
+            runnerThreadSprite.setMessage("RunnerThreadSprite");
+            threadContext.addSprite(runnerThreadSprite);
+            threadContext.addYPixels(10);
+            RunnableSprite runnableSprite  = (RunnableSprite) applicationContext.getBean("runnableSprite");
+            attachAndStartRunnable(runnableSprite);
+            runnableSprite.setMessage("RunnableSprite");
+            threadContext.addSprite(runnableSprite);
+            threadContext.addYPixels(10);
+            FutureSprite futureSprite = (FutureSprite) applicationContext.getBean("futureSprite");
+//            attachAndStartRunnable(futureSprite);
+            futureSprite.setMessage("FutureSprite");
+            threadContext.addSprite(futureSprite);
+            threadContext.addYPixels(10);
+            FutureRunnableSprite futureRunnableSprite = (FutureRunnableSprite) applicationContext.getBean("futureRunnableSprite");
+            attachAndStartRunnable(futureRunnableSprite);
+            futureRunnableSprite.setMessage("FutureRunnableSprite");
+            threadContext.addSprite(futureRunnableSprite);
+            threadContext.addYPixels(10);
+            PooledThreadSprite pooledThreadSprite  = (PooledThreadSprite) applicationContext.getBean("pooledThreadSprite");
+            attachAndStartRunnable(pooledThreadSprite);
+            pooledThreadSprite.setMessage("PooledThreadSprite");
+            threadContext.addSprite(pooledThreadSprite);
+            threadContext.addYPixels(10);
+            GetterThreadSprite getterThreadSprite  = (GetterThreadSprite) applicationContext.getBean("getterSprite");
+            attachAndStartRunnable(getterThreadSprite);
+            getterThreadSprite.setMessage("GetterThreadSprite");
+            threadContext.addSprite(getterThreadSprite);
+            threadContext.addYPixels(10);
+            WriteThreadSprite writeThreadSprite = (WriteThreadSprite) applicationContext.getBean("writeThreadSprite");
+            attachAndStartRunnable(writeThreadSprite);
+            writeThreadSprite.setMessage("WriteThreadSprite");
+            threadContext.addSprite(writeThreadSprite);
+
             // give it an action tag to test for
-            sprite.setAction("someRunningTag");
+            runnerThreadSprite.setAction("someRunningTag");
             // always attach a runnable, and then start the thread
-            sprite.attachAndStartRunnable(()->{
-                while(sprite.getAction().equals("someRunningTag")) {
+            runnerThreadSprite.attachAndStartRunnable(()->{
+                while(runnerThreadSprite.getAction().equals("someRunningTag")) {
                     // Even tho it is consuming CPU, we need to leave it running so as not to change the thread state
                     // in any case, let it yield to running threads
                     Thread.yield();
                 }
-                threadContext.stopThread(sprite);
+                threadContext.stopThread(runnerThreadSprite);
             });
             // Always add the sprite to the thread context.
-            threadContext.addSprite(sprite);
+            threadContext.addSprite(runnerThreadSprite);
         });
         threadContext.addButton("Stop thread", () -> {
             // get a running thread, if any
@@ -54,6 +98,19 @@ public class BasicSlide extends Slide {
 
         threadContext.addButton("reset", this::reset);
         threadContext.setVisible();
+    }
+
+    private void attachAndStartRunnable(ThreadSprite sprite) {
+        sprite.attachAndStartRunnable(()->{
+            Object mutex = new Object();
+            synchronized (mutex) {
+                try {
+                    mutex.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 
